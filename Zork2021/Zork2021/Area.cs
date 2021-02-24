f98458f017cd40d019c9ec5e894926a8d91cff28
Fedtegreven <bbookkaajj@gmail.com>
2021-02-24 13:09:24 +0100

    spilprogrammering

---
+++ Zork2021/Zork2021/Area.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork2021
+{
+    class Area
+    {
+        string description;
+        string name;
+        //todo add list of items
+        //connections as key -> value       directions -> another area
+        Dictionary<Directions, Area> connections = new Dictionary<Directions,Area>();
+
+        public Area(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+
+        public void AddConnection(Directions direction, Area other)
+        {
+            if(!connections.ContainsKey(direction))
+            connections.Add(direction, other);
+        }
+
+        public void AddConnection(Directions direction, Area other, bool bidirectional = false)
+        {
+            if (bidirectional) AddConnectionBidirectional(direction, other);
+            else AddConnection(direction, other);
+        }
+
+        private void AddConnectionBidirectional(Directions direction, Area other)
+        {
+            Directions oppostie;
+            switch(direction)
+            {
+                case Directions.North:
+                    oppostie = Directions.South;
+                    break;
+                case Directions.South:
+                    oppostie = Directions.North;
+                    break;
+                case Directions.West:
+                    oppostie = Directions.East;
+                    break;
+                case Directions.East:
+                    oppostie = Directions.West;
+                    break;
+                default:
+                    oppostie = Directions.South;
+                    break;
+            }
+            //add a connection between this and other with "direction"
+            this.AddConnection(direction, other);
+            //add a copnnection between other and this with "opposite"
+            other.AddConnection(oppostie, this);
+        }
+    }
+
+    public enum Directions
+    {
+        East, West, North, South
+    }
+}
---
+++ Zork2021/Zork2021/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zork2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Area a1 = new Area("test", "");
+            Area a2 = new Area("other", "");
+            //a1.AddConnection(Directions.North, a2);
+            a1.AddConnection(Directions.North, a2, true);
+        }
+    }
+}
