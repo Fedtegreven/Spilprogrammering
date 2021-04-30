1c4915053113e7ea95e383c08945b5256d553220
Fedtegreven <bbookkaajj@gmail.com>
2021-05-01 01:45:36 +0200

    PokemonStuff

--- ProkemonAssignment2/Assets/Scripts/PlayerController.cs
+++ ProkemonAssignment2/Assets/Scripts/PlayerController.cs
@@ -4,39 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float normalSpeed;
+    public float sprintSpeed;
     public float mSpeed;
 
     public bool isMoving;
     private Vector2 input;
 
+    private Animator animatior;
+
+    public LayerMask solidObjectsLayer;
+    public LayerMask tallGrassLayer;
+
     // Start is called before the first frame update
     void Start()
     {
+        sprintSpeed = normalSpeed + 2;
+        mSpeed = normalSpeed;
+        animatior = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        if (Input.GetKey(KeyCode.LeftShift)) mSpeed = sprintSpeed;
+        else mSpeed = normalSpeed;
+
         if (!isMoving)
         {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
-
             // Removes Diagonal Movement
             if (input.x != 0) input.y = 0;
 
             if(input != Vector2.zero)
             {
+                animatior.SetFloat("moveX", input.x);
+                animatior.SetFloat("moveY", input.y);
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
+                if(IsWalkable(targetPos))
                 StartCoroutine(Move(targetPos));
 
             }
 
         }
+        animatior.SetBool("isMoving", isMoving);
         
     }
 
@@ -52,5 +70,27 @@ public class PlayerController : MonoBehaviour
 
         isMoving = false;
 
+        CheckForEncounters();
+    }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if(Physics2D.OverlapCircle(targetPos, 0.15f, solidObjectsLayer) != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    void CheckForEncounters()
+    {
+        if(Physics2D.OverlapCircle(transform.position, 0.2f, tallGrassLayer) != null)
+        {
+            if(Random.Range(1,100 +1) <= 25)
+            {
+                Debug.Log("Wild Pokemon encountered");
+            }
+        }
     }
 }
